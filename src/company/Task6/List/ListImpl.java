b86a0d03d113617e3ab362c1d16e75ef49ea6c9c
spooky230 <spookyeet@gmail.com>
2021-03-24 11:18:30 +0200

    Custom list example

--- src/company/Task6/List/ListImpl.java
+++ src/company/Task6/List/ListImpl.java
@@ -3,56 +3,132 @@ package company.Task6.List;
 public class ListImpl<T> implements List<T>{
     @Override
     public void addFirst(T item) {
+        if (head==null){
+            init(item);
+        }
+        else{
+            Node<T> newHead = new Node<>(item);
+            head.previous = newHead;
+            newHead.next = head;
+            head = newHead;
+        }
 
     }
 
     @Override
     public void addLast(T item) {
-
+        if (head==null){
+            init(item);
+        }
+        else{
+            Node<T> newTail = new Node<>(item);
+            tail.next = newTail;
+            newTail.previous = tail;
+            tail = newTail;
+        }
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        T data = head.data;
+        if(head==tail){
+            remove();
+        }
+        else {
+            Node<T> newHead = head.next;
+            head.next = null;
+            newHead.previous = null;
+            head = newHead;
+        }
+        return data;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        T data = tail.data;
+        if(head==tail){
+            remove();
+        }
+        else {
+            Node<T> newTail = tail.previous;
+            tail.previous = null;
+            newTail.next = null;
+            tail = newTail;
+        }
+        return data;
+    }
+    private void remove() {
+     head = null;
+     tail = null;
+     current = null;
     }
 
     @Override
     public T head() {
-        return null;
+        current = head;
+        return current.data;
     }
 
     @Override
     public T tail() {
-        return null;
+        current = tail;
+        return current.data;
     }
 
     @Override
     public T current() {
-        return null;
+        return current.data;
     }
 
     @Override
     public T next() {
-        return null;
+        if(current.next==null){
+            throw new IndexOutOfBoundsException();
+        }
+        current = current.next;
+        return current.data;
     }
 
     @Override
     public T previous() {
-        return null;
+        if(current.previous==null){
+            throw new IndexOutOfBoundsException();
+        }
+        current = current.previous;
+        return current.data;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index<0){
+            throw new IndexOutOfBoundsException();
+        }
+        head();
+        for (int i = 0; i <index; i++) {
+            next();
+        }
+        return current.data;
     }
 
     @Override
     public int indexOf(T item) {
         return 0;
+    }
+    private Node<T> head;
+    private Node<T> current;
+    private Node<T> tail;
+    private void init(T item){
+        head = new Node<>(item);
+        current = head;
+        tail = head;
+    }
+    private class Node<T>{
+        private T data;
+
+        public Node(T data) {
+            this.data = data;
+        }
+        private Node<T>next;
+        private Node<T>previous;
     }
 }
--- src/company/Task6/Main6.java
+++ src/company/Task6/Main6.java
@@ -1,4 +1,15 @@
 package company.Task6;
 
+import company.Task6.List.ListImpl;
+
 public class Main6 {
+    public static void main(String[] args) {
+        ListImpl<String>stringList = new ListImpl<>();
+        stringList.addLast("AAA");
+        stringList.addLast("BBB");
+        stringList.addLast("CCC");
+        stringList.addLast("DDD");
+        System.out.println(stringList.get(-10));
+        System.out.println(stringList.get(-20));
+    }
 }
