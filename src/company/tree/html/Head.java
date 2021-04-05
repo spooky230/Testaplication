e76b16ed5e1c8c00d0d4a89d5d303e2fe70911f8
Microso <aasdf@gmail.com>
2021-04-05 11:01:32 +0300

    commit message

--- src/company/tree/html/Head.java
+++ src/company/tree/html/Head.java
@@ -2,16 +2,35 @@ package company.tree.html;
 import java.util.LinkedList;
 import java.util.List;
 public class Head extends HtmlElement{
+    public static final String TITLE_S_TITLE = "<title>%s</title>%n";
+    public static final String S_SCRIPT = "%s<script type = \"text/javaScript\" src=\"%s\">%s</script>%n";
+    public static final String SRC_S = "link type=\"text/css\" rel=\"stylesheet\" src=\"%s\">%n";
     public String title;
     List<String> css = new LinkedList<String>();
-    List<String> Scripts = new LinkedList<String>();
-    public Head(String title,List<String> css,List<String> Scripts){
+    List<String> scripts = new LinkedList<String>();
+    public Head(String title,List<String> css,List<String> scripts){
         this();
         this.title = title;
         this.css = css;
-        this.Scripts = Scripts;
+        this.scripts = scripts;
     }
     public Head(){
         this.tagName = "Head";
+    }
+    @Override
+    public String render() {
+        String open =renderOpenedTag();
+        String titleRender = String.format(TITLE_S_TITLE, title);
+        String scriptsRender = "";
+        for (int i = 0; i < scripts.size(); i++) {
+            scriptsRender= String.format(S_SCRIPT, scriptsRender, scripts.get(i), text);
+        }
+        String linksRender = "";
+        for (int i = 0; i < css.size(); i++) {
+            linksRender += String.format(SRC_S, css.get(i));
+        }
+        String close = renderClosedTag();
+        String result =open + titleRender + scriptsRender +linksRender + close;
+        return result;
     }
 }
